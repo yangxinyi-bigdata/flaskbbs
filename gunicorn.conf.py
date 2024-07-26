e0b083122b81fc894e5fc23d11575da152fae221
yangxinyi-bigdata <yangyuehaha@qq.com>
2024-07-26 17:40:43 +0800

    gunicor配置修改

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,7 @@
 import multiprocessing
 
-bind = "127.0.0.1:5000"
+bind = "127.0.0.1:5000" # 这样只能在内网访问
+# bind = "0.0.0.0:5000"  # 这样可以在外网访问, 但是使用nginx不需要
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 10
 accesslog = "/var/log/pythonbbs/gunicorn_access.log"
